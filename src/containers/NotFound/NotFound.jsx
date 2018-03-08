758cc5f237f0797590589c464c735e8d6a7cfbe8
sqrtthree <imsqrtthree@gmail.com>
2018-03-08 23:28:26 +0800

    :sparkles: Add 404 page

--- src/containers/NotFound/NotFound.jsx
+++ src/containers/NotFound/NotFound.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { Link } from 'react-static'
 import styles from './NotFound.css'
 
+import errorImage from '../../static/404.svg'
+
 export default () => (
-  <div className="notFound"></div>
+  <div className="not-found">
+    <img src={errorImage} alt="404"/>
+  </div>
 )
